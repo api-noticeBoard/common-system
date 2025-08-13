69a7cf21d7e5d87571d5268a573072e1bc76442a
jimiboy <jimi008285@gmail.com>
2025-08-13 16:16:53 +0900

    Change Package name

--- src/main/java/com/portfolio/commsys/logging/LoggingAspect.java
+++ src/main/java/com/portfolio/commsys/logging/LoggingAspect.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
 @Slf4j      // Lombok의 Slf4j를 사용해 로거 자동 생성
@@ -13,15 +12,13 @@ import org.springframework.util.StopWatch;
 public class LoggingAspect {
 
     // Advice: 적용 지점에서 실행 로직
-    @Around("execution(com.portfolio.syscomm..*Service.*(..))")
-//    @Around("annotation(com.portfolio.syscomm.logging.aop.annotation.Loggable)")
+    @Around("execution(* com.portfolio.syscomm..*Service.*(..))")
     public Object logMethodExecution(ProceedingJoinPoint jointPoint) throws Throwable {
 
         // 메서드 정보 추출
         String methodName = jointPoint.getSignature().toShortString();
-//        Object[] args = jointPoint.getArgs();
-//        log.info("[logMethodExecution] Method Start : {}, Args : {}", methodName, args);
-        log.info("==> Method Start: {}", methodName);
+        Object[] args = jointPoint.getArgs();
+        log.info("==> Method Start: {}, Args: {}", methodName, args);
 
         // 실행 시간 측정 시작
         StopWatch stopWatch = new StopWatch();
